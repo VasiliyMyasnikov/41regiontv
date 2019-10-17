7c765f2b9f8cdb9e4e40b5fb52208a5272b2d78b
VasiliyMyasnikov <vmweb41@gmail.com>
2019-10-18 01:07:56 +1200

    Прикрутил Yandex map api

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
     elementsHeightChange();
+
 });
+
+
 
 $(window).resize(function(){
     elementsHeightChange();
-    console.log(123);
+
 });
 
 
@@ -14,8 +17,73 @@ function elementsHeightChange(){
       var curElementHeight = $(this).height();
       if(elementHeight < curElementHeight){
         elementHeight = curElementHeight;
-        console.log(elementHeight);
+
       }
   });
     $(".about-card").height(elementHeight);
 }
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [53.058741, 158.632688],
+            zoom: 16,
+            behaviors: ['multiTouch']
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        );
+
+       //  myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+       //    geometry: {
+       //         type: "Point",
+       //         coordinates: [53.059464, 158.634579],
+       //         iconCaption: 'Очень длиннный, но невероятно интересный текст'
+       //     },
+       //
+       //
+       //
+       // }, {
+       //     // Опции.
+       //     // Иконка метки будет растягиваться под размер ее содержимого.
+       //     preset: 'islands#blackStretchyIcon',
+       //      iconColor: 'orange',
+       //     // Метку можно перемещать.
+       //     draggable: false
+       // });
+
+
+
+    myMap.geoObjects
+        .add(new ymaps.Placemark([53.059464, 158.634577], {
+            iconCaption: '41 Регион'
+        }, {
+            preset: 'islands#blueHeartIcon',
+            iconColor: 'orange'
+        }))
+
+});
+
+
+
+
+
+// ymaps.ready(init);
+// function init(){
+// var myMap = new ymaps.Map("map", {
+//     center: [53.059394, 158.634600],
+//     zoom: 17
+// });
+// myMap.geoObjects
+//     .add(myGeoObject)
+//     .add(myPieChart)
+//     .add(new ymaps.Placemark([53.059394, 158.634600], {
+//         balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
+//     }, {
+//         preset: 'islands#icon',
+//         iconColor: '#0095b6'
+//     }))
+// }
