c5cf8f7232b9217d2da1eaf08b69eddfee8aea88
VasiliyMyasnikov <vmweb41@gmail.com>
2019-10-18 01:17:03 +1200

    Работа с картой

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ function elementsHeightChange(){
 
 ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
-            center: [53.058741, 158.632688],
+            center: [53.059248, 158.634287],
             zoom: 16,
             behaviors: ['multiTouch']
         }, {
@@ -37,25 +37,6 @@ ymaps.ready(function () {
             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
         );
 
-       //  myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-       //    geometry: {
-       //         type: "Point",
-       //         coordinates: [53.059464, 158.634579],
-       //         iconCaption: 'Очень длиннный, но невероятно интересный текст'
-       //     },
-       //
-       //
-       //
-       // }, {
-       //     // Опции.
-       //     // Иконка метки будет растягиваться под размер ее содержимого.
-       //     preset: 'islands#blackStretchyIcon',
-       //      iconColor: 'orange',
-       //     // Метку можно перемещать.
-       //     draggable: false
-       // });
-
-
 
     myMap.geoObjects
         .add(new ymaps.Placemark([53.059464, 158.634577], {
@@ -66,24 +47,3 @@ ymaps.ready(function () {
         }))
 
 });
-
-
-
-
-
-// ymaps.ready(init);
-// function init(){
-// var myMap = new ymaps.Map("map", {
-//     center: [53.059394, 158.634600],
-//     zoom: 17
-// });
-// myMap.geoObjects
-//     .add(myGeoObject)
-//     .add(myPieChart)
-//     .add(new ymaps.Placemark([53.059394, 158.634600], {
-//         balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
-//     }, {
-//         preset: 'islands#icon',
-//         iconColor: '#0095b6'
-//     }))
-// }
